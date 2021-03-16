d5136fd0c242aeedf109cff1dff4bae763673d46
Katherine Rodriguez <krodr249@fiu.edu>
2021-03-16 19:32:40 -0400

    Populated side pages, nav bar functionality

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
   res.render("home", {homeStartingContent:homeStartingContent});
 })
-
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent:aboutContent});
+})
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent:contactContent});
+})
 
 
 app.listen(3000, function() {
